5f824895ea0b3e2012529a61bbdbefac394eb404
student1304 <student1304@online.de>
2020-07-09 13:28:00 +0200

    klimat scraper

---
+++ app/appdev.py
@@ -0,0 +1,37 @@
+import streamlit as st
+import pandas as pd
+#import numpy as np
+#import time
+from api_tools import get_product_info
+
+
+# load md text from file and display on page
+path_to_md = "./intro.md"
+with open(path_to_md, "r") as f:
+    md_text = f.read()
+st.markdown(md_text)
+
+# load and display cart
+cart_csv = st.file_uploader('Drag&Drop CSV oder Upload hier', type="csv")
+if cart_csv is not None:
+    data = pd.read_csv(cart_csv, sep=',', decimal='.')
+    st.write(data)
+    st.balloons()
+
+
+    #loop through cart and show categories
+    for gtin in data.GTIN:
+        #st.write(gtin)
+        info = get_product_info(gtin)
+        st.info('getting info for... '+str(gtin))
+        if info['status'] != 1:
+            st.markdown('   - **no info** available in openfoodfacts.org')
+        else:
+            image_url = info['product']['selected_images']['front']['thumb']['de']
+            st.markdown('![Image](%s)' %image_url)
+            st.write(
+                info['product']['packaging_tags'], 
+                info['product']['categories_hierarchy']
+            )
+    st.success('it worked!')
+        
---
+++ app/eua_tools.py
@@ -0,0 +1,41 @@
+from bs4 import BeautifulSoup
+import requests
+from sqlite_cache.sqlite_cache import SqliteCache
+import time
+
+cache = SqliteCache('../data/webcache')
+url = "https://markets.businessinsider.com/commodities/co2-european-emission-allowances/euro"
+timestamp = time.strftime("%Y-%M-%d", time.localtime())
+key = "eua_"+timestamp
+
+def get_eua_price(key):
+    '''
+    Scrapes EUA price from website if not stored in Cache
+    Returns price of EUA in EUR/t
+    '''
+    r = requests.get(url)
+    if r.status_code == 200:
+        soup = BeautifulSoup(r.content, 'html.parser')
+        a = soup.find('span', class_="aktien-big-font")
+        eua = float(a['data-jsvalue'])
+        print('EUA today:', eua)
+        cache.set(key, eua)
+        return eua
+    if r.status_code != 200:
+        eua = 25.0 #update to get last available value of ask other source
+        return eua
+
+def fetch_eua_price(key='eua_'+time.strftime("%Y-%M-%d", time.localtime())):
+    '''
+    Returns current EUA price (EUR / t) . Checks local cache first, then requests website if not yet stored
+    
+    Usage: eua= return_eua_price(key)
+        default key is 'eua_YYYY-MM-DD', leave emtpy for default or enter other date
+    '''
+    eua = cache.get(key)
+    print(eua, 'in cache')
+    if eua is None:
+        print('requesting from website...')
+        eua = get_eua_price(key)
+    return eua
+
